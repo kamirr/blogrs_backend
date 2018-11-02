8585406831c9c1a0d4c2bd6beb0a6e6cd9c825d7
Kamil Koczurek <koczurekk@gmail.com>
2018-11-02 20:07:29 +0100

    Make server fetch data from StaticContent by default

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+#![feature(label_break_value)]
+
 extern crate hyper;
+
+mod static_content;
 mod service;
 
+use crate::static_content::StaticContent;
 use hyper::service::service_fn_ok;
+use std::sync::{Mutex, Arc};
 use hyper::rt::Future;
 use hyper::Server;
 
-fn main() {
+fn run_server(sc: Arc<Mutex<StaticContent>>) {
     let port = 3000;
     let addr = ([127, 0, 0, 1], port).into();
 
-    let new_svc = || {
-        service_fn_ok(service::make_service)
+    let sc = Arc::clone(&sc);
+    let new_svc = move || {
+        let sc = Arc::clone(&sc);
+        service_fn_ok(move |req| {
+            let sc = Arc::clone(&sc);
+            service::serve(req, sc)
+        })
     };
 
     let server = Server::bind(&addr)
@@ -18,4 +29,8 @@ fn main() {
         .map_err(|e| eprintln!("server error: {}", e));
 
     hyper::rt::run(server);
+}
+
+fn main() {
+    run_server(Arc::new(Mutex::new(StaticContent::new("."))));
 }
--- src/service.rs
+++ src/service.rs
@@ -1,27 +1,36 @@
 use hyper::{StatusCode, Method, Body, Request, Response};
+use crate::static_content::StaticContent;
+use std::sync::{Mutex, Arc};
 
-fn invalid_request() -> Response<Body> {
+fn invalid_request(code: u16) -> Response<Body> {
     Response::builder()
-        .status(StatusCode::from_u16(422).unwrap())
+        .status(StatusCode::from_u16(code).unwrap())
         .body(Body::empty())
         .unwrap()
 }
 
-fn get(uri: String) -> Response<Body> {
-    Response::new(Body::from(uri))
+fn get(uri: String, cont: Arc<Mutex<StaticContent>>) -> Response<Body> {
+    let mut lock = (*cont).lock().unwrap();
+    let res = (*lock).fetch(uri);
+    drop(lock);
+
+    match res {
+        Some(s) => Response::new(Body::from(s)),
+        None => invalid_request(404)
+    }
 }
 
 fn put(_: String) -> Response<Body> {
     Response::new(Body::empty())
 }
 
-pub fn make_service(req: Request<Body>) -> Response<Body> {
+pub fn serve(req: Request<Body>, cont: Arc<Mutex<StaticContent>>) -> Response<Body> {
     let uri = req.uri().path().to_string();
     let method = req.method();
 
     match method {
-        &Method::GET => get(uri),
+        &Method::GET => get(uri, cont),
         &Method::PUT => put(uri),
-        _ => invalid_request()
+        _ => invalid_request(422)
     }
 }
