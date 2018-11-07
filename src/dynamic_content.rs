75233319dd4e10d18bb95280303b0a580f5181d9
Kamil Koczurek <koczurekk@gmail.com>
2018-11-07 09:31:04 +0100

    Removed 'path' field from DynamicContent

--- src/dynamic_content.rs
+++ src/dynamic_content.rs
@@ -1,21 +1,20 @@
 #[derive(Clone)]
-pub struct DynamicContent {
-    path: String
-}
+pub struct DynamicContent { }
 
 impl DynamicContent {
-    pub fn new(path: &str) -> Self {
-        DynamicContent {
-            path: path.to_string()
-        }
+    pub fn new() -> Self {
+        DynamicContent { }
     }
 
     pub fn fetch_post(&self, id: &String) -> Option<String> {
-        Some("text".to_string())
+        Some(format!("post: {}", id))
     }
 
-    pub fn fetch(self, mut file: String) -> Option<String> {
-        let dirs: Vec<String> = file.split("/").map(|s| s.to_string()).collect();
+    pub fn fetch(self, file: String) -> Option<String> {
+        let mut dirs: Vec<String> = file.split("/").map(|s| s.to_string()).collect();
+        dirs.remove(0);
+
+        println!("{:?}", dirs);
 
         if dirs.len() < 2 {
             return None;
@@ -26,7 +25,7 @@ impl DynamicContent {
                 if dirs.len() < 3 {
                     None
                 } else {
-                    self.fetch_post(&dirs[3])
+                    self.fetch_post(&dirs[2])
                 }
             },
             _ => None
