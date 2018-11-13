4f10ee74cc668a1708bcc78b6a6bb45124880ff1
Kamil Koczurek <koczurekk@gmail.com>
2018-11-13 11:32:01 +0100

    Moved run_server(Content) to run_server.rs

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,16 @@ extern crate hyper;
 mod dynamic_content;
 mod static_content;
 mod manage_posts;
+mod run_server;
 mod connection;
 mod content;
 mod service;
 mod schema;
 mod models;
 
-use self::connection::establish_sql_connection;
-use hyper::service::service_fn_ok;
+use crate::run_server::run_server;
 use crate::content::Content;
-use hyper::rt::Future;
-use hyper::Server;
-
-fn run_server(cont: Content) {
-    let port = 3000;
-    let addr = ([127, 0, 0, 1], port).into();
-
-    let new_svc = move || {
-        let cont = cont.clone();
-        service_fn_ok(move |req| {
-            service::serve(req, cont.clone())
-        })
-    };
-
-    let server = Server::bind(&addr)
-        .serve(new_svc)
-        .map_err(|e| eprintln!("server error: {}", e));
-
-    hyper::rt::run(server);
-}
 
 fn main() {
-    let _connection = establish_sql_connection();
     run_server(Content::new("frontend/"));
 }
