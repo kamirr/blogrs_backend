e3aaa0ce5329d74c51d81efa675f9eb90e940dd7
Kamil Koczurek <koczurekk@gmail.com>
2018-11-13 18:36:12 +0100

    Added dependency on rand

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(label_break_value)]
+#![feature(slice_concat_ext)]
 #![allow(proc_macro_derive_resolution_fallback)]
 
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
 extern crate hyper;
+extern crate rand;
 extern crate sha2;
 
 mod dynamic_content;
