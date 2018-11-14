25911e49b88575772dfff801ed1937570b1c2e13
Kamil Koczurek <koczurekk@gmail.com>
2018-11-14 23:18:50 +0100

    Code cleanup in login.rs

--- src/login.rs
+++ src/login.rs
@@ -12,7 +12,7 @@ pub struct LoginData {
     pub hash: String
 }
 
-fn test_hash(l_hash: String, p_hash: String, data: LoginData, conn: &MysqlConnection) -> Option<AuthKey> {
+fn test_hash(l_hash: String, p_hash: String, data: LoginData) -> bool {
     let mut hasher = Sha224::new();
     hasher.input(format!("{}{}", l_hash, p_hash));
 
@@ -29,10 +29,7 @@ fn test_hash(l_hash: String, p_hash: String, data: LoginData, conn: &MysqlConnec
         })
         .collect::<String>();
 
-    match client_hash == local_hash {
-        true => Some(generate_auth_key(conn)),
-        false => None
-    }
+    client_hash == local_hash
 }
 
 fn fetch_from_nonrepeating(key: &str, conn: &MysqlConnection) -> Result<Vec<Nonrepeating>, ()> {
@@ -48,26 +45,34 @@ fn fetch_from_nonrepeating(key: &str, conn: &MysqlConnection) -> Result<Vec<Nonr
     }
 }
 
-#[post("/", data = "<data>", format = "json")]
-pub fn login(data: Json<LoginData>, conn: State<SafeConnection>) -> Option<AuthKey> {
-    let lock = (*conn).lock().unwrap();
-    let conn = &*lock;
-
+fn test_login(data: Json<LoginData>, conn: &MysqlConnection) -> bool {
     let login_hash = match fetch_from_nonrepeating("login_hash", conn) {
         Ok(hash) => match hash.len() {
             1 => hash[0].title.clone(),
-            _ => return Some(generate_auth_key(conn))
+            _ => return true
         },
-        Err(_) => return None
+        Err(_) => return false
     };
 
     let pass_hash = match fetch_from_nonrepeating("pass_hash", conn) {
         Ok(hash) => match hash.len() {
             1 => hash[0].title.clone(),
-            _ => return Some(generate_auth_key(conn))
+            _ => return true
         },
-        Err(_) => return None
+        Err(_) => return false
     };
 
-    test_hash(login_hash, pass_hash, data.into_inner(), conn)
+    test_hash(login_hash, pass_hash, data.into_inner())
+}
+
+#[post("/", data = "<data>", format = "json")]
+pub fn login(data: Json<LoginData>, conn: State<SafeConnection>) -> Option<AuthKey> {
+    let conn: &SafeConnection = &conn;
+    let lock = (*conn).lock().unwrap();
+
+    if test_login(data, &*lock) {
+        Some(generate_auth_key(&*lock))
+    } else {
+        None
+    }
 }
