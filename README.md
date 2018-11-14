9d81f86a1e7f6c623a7b737baed34d17af3b64ed
Kamil Koczurek <koczurekk@gmail.com>
2018-11-14 19:07:38 +0100

    Ported code from Hyper to Rocket

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # blog.rs
-Simple single-user blogging system written in [Rust](https://www.rust-lang.org/en-US/) using  [hyper](hyper.rs) and [diesel](diesel.rs).
+Simple single-user blogging system written in [Rust](https://www.rust-lang.org/en-US/) using  [Rocket](rocket.rs) and [Diesel](diesel.rs).
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
+#![feature(proc_macro_hygiene, decl_macro)]
 #![feature(label_break_value)]
 #![feature(slice_concat_ext)]
 #![allow(proc_macro_derive_resolution_fallback)]
 
-#[macro_use]
-extern crate diesel;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate rocket;
+extern crate rocket_contrib;
 extern crate dotenv;
-extern crate hyper;
 extern crate rand;
 extern crate sha2;
 
-mod dynamic_content;
-mod static_content;
 mod authentication;
 mod manage_posts;
-mod run_server;
 mod connection;
-mod content;
-mod service;
 mod schema;
 mod models;
 
-use crate::run_server::run_server;
-use crate::content::Content;
+fn parse_entry_templ(entry: &String, id: u64) -> Option<String> {
+    Some(entry.replace(
+        "{ID}",
+        &format!("{}", id)
+    ))
+}
+
+#[get("/post/<id>")]
+fn post_templ(id: u64) -> Option<String> {
+    let entry = "frontend/templates/entry.html";
+    match std::fs::read_to_string(entry) {
+        Ok(text) => parse_entry_templ(&text, id),
+        _ => None
+    }
+}
 
 fn main() {
-    run_server(Content::new("frontend/"));
+    use rocket_contrib::serve::StaticFiles;
+
+    rocket::ignite()
+        .mount("/", StaticFiles::from("frontend/"))
+        .mount("/", routes![post_templ])
+        .launch();
 }
