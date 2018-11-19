5fe3f58987e238b0f9fe435c2550cee647406299
Kamil Koczurek <koczurekk@gmail.com>
2018-11-19 19:00:23 +0100

    A bit more refactoring

--- src/auth.rs
+++ src/auth.rs
@@ -1,9 +1,6 @@
-use crate::db::models::Nonrepeating;
 use crate::db::connection::*;
+use crate::db::special::*;
 
-use diesel::mysql::MysqlConnection;
-use diesel::insert_into;
-use diesel::prelude::*;
 use rocket::State;
 use rand::random;
 
@@ -25,60 +22,16 @@ fn random_hex(count: u32) -> String {
     res
 }
 
-fn save_key_in_db(key: &str, conn: &MysqlConnection) {
-    use crate::db::schema::nonrepeating::dsl::*;
-    use diesel::dsl::exists;
-    use diesel::dsl::select;
-
-    let db_key = "current_auth";
-
-    let to_insert = Nonrepeating {
-        id: db_key.to_string(),
-        value: key.into()
-    };
-
-    let should_delete = select(exists(
-            nonrepeating.filter(id.eq(db_key))
-        ))
-        .get_result(conn)
-        .unwrap();
-
-    if should_delete {
-        diesel::delete(nonrepeating.filter(
-                id.eq(db_key)
-            ))
-            .execute(conn)
-            .unwrap();
-    }
-
-    insert_into(nonrepeating)
-        .values(&to_insert)
-        .execute(conn)
-        .unwrap();
-}
-
 pub fn verify_auth_key(key: AuthKey, pool: &State<Pool>) -> bool {
-    use crate::db::schema::nonrepeating::dsl::*;
-    use diesel::dsl::exists;
-    use diesel::dsl::select;
-
-    let db_key = "current_auth";
-    let conn = pool.get().unwrap();
-
-    let ok = select(exists(
-            nonrepeating
-                .filter(id.eq(db_key))
-                .filter(value.eq(key))
-        ))
-        .get_result(&conn)
-        .unwrap();
-
-    ok
+    match Special::new(pool).get("current_auth") {
+        Some(key_in_db) => key == key_in_db,
+        None => false
+    }
 }
 
 pub fn generate_auth_key(pool: &State<Pool>) -> AuthKey {
     let res = random_hex(6);
-    save_key_in_db(&res, &pool.get().unwrap());
+    Special::new(pool).set("current_auth", &res).unwrap();
 
     res
 }
